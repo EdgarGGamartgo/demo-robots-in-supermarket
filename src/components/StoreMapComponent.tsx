761eea80e39ac21ec3472b82a7b42ab6efd86d12
EdgarGGamartgo <edgarggamartgo@gmail.com>
2022-05-27 21:42:23 -0500

    init

--- src/components/StoreMapComponent.tsx
+++ src/components/StoreMapComponent.tsx
@@ -1,22 +1,90 @@
-import React, { ReactElement, useRef } from "react";
+import React, {
+  ReactElement,
+  useRef,
+  useEffect,
+  useState,
+  useLayoutEffect,
+} from "react";
 import PrismaZoom from "react-prismazoom";
-import { StoreMapComponentStyles as Styles } from "../styles";
+import { Product } from "../models";
 
 export default function StoreMapComponent(): ReactElement {
-  const mapElement = useRef<ReactElement>(null);
+  const mapElement = useRef<any>(null);
+
+  const [products, setProducts] = useState<Product[]>([]);
+
+  const [viewportSize, setViewportSize] = useState<number[]>([0, 0]);
+
+  const [mapSize, setMapSize] = useState<{ y: number; x: number }>({
+    x: 0,
+    y: 0,
+  });
 
   const onZoomChange = (zoom: number): void => {};
 
+  const setProductPositionX = (x: number): string => {
+    if (x === 100) return `${Math.floor(mapSize.x - 6)}px`;
+    return `${Math.floor((x * mapSize.x) / 100)}px`;
+  };
+
+  const setProductPositionY = (y: number): string => {
+    if (y === 100) return `${Math.floor(mapSize.y - 6)}px`;
+    return `${Math.floor((y * mapSize.y) / 100)}px`;
+  };
+
+  useLayoutEffect(() => {
+    const updateSize = () => {
+      setViewportSize([window.innerWidth, window.innerHeight]);
+    };
+
+    window.addEventListener("resize", updateSize);
+
+    updateSize();
+
+    return () => window.removeEventListener("resize", updateSize);
+  }, []);
+
+  useEffect(() => {
+    const x = mapElement?.current?.ref?.current?.clientWidth;
+    const y = mapElement?.current?.ref?.current?.clientHeight;
+
+    if (x >= 0 && y >= 0) {
+      setMapSize({
+        x,
+        y,
+      });
+    }
+  }, [viewportSize]);
+
+  useEffect(() => {
+    setProducts([
+      {
+        id: 1,
+        x: 50,
+        y: 90,
+      },
+    ]);
+  }, []);
+
   return (
-    <div className="w-screen h-screen bg-indigo-900 flex flex-col justify-center items-center">
-      <div className="bg-indigo-900 w-[90%] h-[90%] relative rounded-2xl overflow-hidden">
+    <div className="w-screen h-screen bg-blue-900 flex flex-col justify-center items-center">
+      <div className="bg-indigo-900 w-[90%] h-[90%] relative overflow-hidden">
         <PrismaZoom
-          className="bg-neutral-400 w-[100%] h-[100%] rounded-2xl"
+          className="bg-neutral-400 w-[100%] h-[100%]"
           ref={mapElement}
           maxZoom={8}
           onZoomChange={onZoomChange}
         >
-          <span className={Styles.ProductLocationStyles}></span>
+          {products.map(({ x, y }, i) => (
+            <span
+              key={i}
+              className={`absolute h-[6px] w-[6px] bg-sky-800 rounded-full`}
+              style={{
+                left: setProductPositionX(x),
+                bottom: setProductPositionY(y),
+              }}
+            ></span>
+          ))}
         </PrismaZoom>
       </div>
     </div>
--- src/styles/StoreMapComponentStyles.ts
+++
@@ -1,4 +0,0 @@
-export const StoreMapComponentStyles = {
-  ProductLocationStyles:
-    "absolute h-[1em] w-[1em] bg-sky-800 rounded-full top-[3em] right-[3em]",
-};
